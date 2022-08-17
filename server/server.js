c45387b0de8a8c1cfe17c92d5c951028901330a9
Athibdee <s6401012620153@email.kmutnb.ac.th>
2022-08-17 13:43:21 +0700

    BACKEND

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const app = express()
 mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     useUnifiedTopology:false
-}).then(()=>console.log("เชื่อมต่อเรียบร้อย"))
+})
+.then(()=>console.log("เชื่อมต่อเรียบร้อย"))
 .catch((err)=>console.log(err))
 
 // middle - ware
