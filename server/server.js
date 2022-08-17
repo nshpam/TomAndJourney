452e542a79becc207c7fc6d038f85c13cdb227e7
Athibdee <s6401012620153@email.kmutnb.ac.th>
2022-08-17 14:03:12 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-
-const mongoose = require("mongoose") 
-// mongoose ไว้เชื่อม mongoDB
+const mongoose = require("mongoose")  // mongoose ไว้เชื่อม mongoDB
 
 require("dotenv").config()
 // ใช้ตั้งค่าไฟล์นามสกุล .env
+
+const blogRoute = require('./route/blog.js')
+// สร้าง func BlogRoute โดยดึง blog.js มากจากfloder route
 
 const app = express()
 // connect cloud database
@@ -23,11 +24,9 @@ app.use(cors()) //
 app.use(morgan("dev")) //
 
 // route
-app.get("*",(req,res)=>{
-    res.json({
-        data:"message from server"
-    })
-})
+app.use('/api',blogRoute)
 
+
+//Setting Port
 const port = process.env.PORT || 8080
 app.listen(port,()=>console.log(`start server in port ${port}`))
