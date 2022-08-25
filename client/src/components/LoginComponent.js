5ffb433e24aa63a276cb7f4d6cee6ff129b061cd
Athibdee <s6401012620153@email.kmutnb.ac.th>
2022-08-26 05:12:24 +0700

    LOGINFRONTEND

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -2,13 +2,34 @@ import './Login.css';
 
 const LoginComponent=()=>{
     return (
-        <div class="form">
+        <div class="form-login">
+            <h1 id="title-login">LOGIN</h1>
 
-        <label for="email">Email</label>
-        <input type="email" id="email"></input>
-        <label for="password">Password</label>
-        <input type="password" id="password"></input>
-        <input type="submit" id="submit" value="Submit"></input>
+            <div>
+                <label for="email">Email</label>
+                <input type="email" id="email"></input>
+            </div>
+
+            <div>
+                <label for="password">Password</label>
+                <input type="password" id="password"></input>
+            </div>
+            
+            <a href="#" id="forgetpass">Forget password</a>
+            <span id="remem-check"> 
+                <input type="checkbox" id="checkbox-remember">
+                    </input> 
+                    Remember me.  
+                 </span> 
+            
+
+            <div>
+                <input type="submit" id="submit" value="Login"></input>
+            </div>
+
+            
+
+                
         </div>
 
     );
