297e7623ca705cedc65be13931a3aa6534ba0d37
Athibdee <s6401012620153@email.kmutnb.ac.th>
2022-10-16 11:47:37 +0700

    not

--- client_react/src/components/NavbarComponent.js
+++ client_react/src/components/NavbarComponent.js
@@ -1,17 +1,19 @@
-import './Navbar.css';
 const NavbarComponents=()=>{
 
     return(
-        <header id="Nav_header">
-        <a class="title" id="logo" >Tom And Journey</a>
+        <header className="Nav-header">
+        <a class="title" id="Nav_" >Tom And Journey</a>
         
         <nav>
-          <ul class="nav">
-            <li > <a href="/#" id="Nav_">Home</a> </li>
+
+          <ul class="nav__links">
+            <li> <a href="/" id="Nav_">Home</a> </li>
+            <li> <a href="/abouts" id="Nav_">About us</a> </li>
             <li> <a href="#" id="Nav_">Trip Planer</a> </li>
-            <li> <a href="/#" id="Nav_">Login</a> </li>
+
           </ul>
         </nav>
+        <a class="sgn" href="/login" > <button id="signin"> Sign in </button></a>
         
         
       </header>
