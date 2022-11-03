8e04bb63d3bf9df9a37a0a145c1011a5bdc85887
Athibdee <s6401012620153@email.kmutnb.ac.th>
2022-11-03 10:36:35 +0700

    fix SweetAlert

--- client_php/tom-and-journey/admin/config/alert_box.php
+++ client_php/tom-and-journey/admin/config/alert_box.php
@@ -4,6 +4,7 @@
 
     <!-- import sweetalert.js -->
     <script src="js/sweetalert.js"></script>
+    <script src="admin/js/sweetalert.js"></script>
 
     <!-- get data from another file -->
     <?php
